f7a62e2ba682f59835357ce8296c28712de21e7c
Majka111 <145587851+Majka111@users.noreply.github.com>
2023-09-21 18:23:14 +0200

    Update PASCAL_FC_P_A.dpr

--- main/src/PASCAL_FC_P_A.dpr
+++ main/src/PASCAL_FC_P_A.dpr
@@ -1,1 +1,16 @@
+program PASCAL_FC_P_A;
 
+uses
+  Vcl.Forms,
+  PASCAL_FC_A in 'PASCAL_FC_A.pas' {Form1},
+  CisloRadku in 'CisloRadku.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
